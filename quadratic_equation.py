a4732050b30e09638ac7d5c666dc1b5a8f1e4677
Malemute <erlinn@fromru.com>
2017-10-26 21:58:08 +0300

    Более понятные имена переменных

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,13 +2,12 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    sqroot = 0
     if discriminant < 0:
         return None, None
-    sqroot = sqrt(discriminant)
-    root1 = (-b- sqroot) / (2 * a)
+    square_root = sqrt(discriminant)
+    root1 = (-b- square_root) / (2 * a)
     if discriminant == 0:
         return root1, None
     else:
-        root2 = (-b + sqroot) / (2 * a)
+        root2 = (-b + square_root) / (2 * a)
         return root1, root2
